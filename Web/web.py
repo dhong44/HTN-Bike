d852869a553ae8f875d48a7f45c1551b273519cd
Siddhant Bhatnagar <sidb97@gmail.com>
2016-09-17 10:56:40 -0400

    Add Distance to Server

--- Web/web.py
+++ Web/web.py
@@ -14,11 +14,28 @@ class Bike:
                  print self.id, self.vehicle_type, self.latitude, self.longitude, self.speed, self.direction, self.status, self.time
 
         def csv(self):
-                string = str(self.id) + ', ' + str(self.vehicle_type) + ', ' + str(self.latitude) + ', ' + str(self.longitude) + ', ' + str(self.speed) + ', ' + str(self.direction) + ', ' + str(self.status) + ', ' + str(self.time)
+                string = str(self.id) + ', ' + str(self.vehicle_type) + ', ' + str(self.latitude) + ', ' + str(self.longitude) + ', ' + str(self.speed) + ', ' + str(self.direction) + ', ' + str(self.status)
                 return string 
 
 bikes = []
-
+def distance(lat1, long1, lat2, lon2):
+		R = 6373.0
+		
+		lat1 = radians(lat1)
+		lon1 = radians(lon1)
+		lat2 = radians(lat2)
+		lon2 = radians(lon2)
+		
+		dlon = lon2 - lon1
+		dlat = lat2 - lat1
+		
+		a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+		c = 2 * atan2(sqrt(a), sqrt(1 - a))
+		
+		distance = R * c
+		return distance
+		
+	
 @app.route('/')
 def index():
         for i in bikes:
